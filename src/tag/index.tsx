22ee9d42b6bcf0f143cd66b1e6b19989471a2f55
huangziyi01 <huangziyi01@baidu.com>
2016-04-11 18:02:15 +0800

    quick push

---
+++ src/tag/index.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react'
+import * as classNames from 'classnames'
+import * as module from './module'
+import {others} from '../../../../common/transmit-transparently/src'
+import './index.scss'
+
+export default class Tag extends React.Component<module.PropsInterface,module.StateInterface> {
+    static defaultProps = new module.Props()
+    public state = new module.State()
+    props:module.PropsInterface
+
+    constructor(props:any) {
+        super(props)
+    }
+
+    handleBeforeClose() {
+        this.props.beforeClose(()=> {
+            this.handleDoClose()
+        }) ? this.handleDoClose() : null
+    }
+
+    handleDoClose() {
+        this.setState({
+            closed: true
+        }, ()=> {
+            this.props.onClosed()
+        })
+    }
+
+    render() {
+        const close = this.props.closable ?<i className="fa fa-close tag-close"
+                                              onClick={this.handleBeforeClose.bind(this)}/> : ''
+        const classes = classNames({
+            '_namespace': true,
+            [this.props['className']]: !!this.props['className']
+        })
+        return (
+            <span>
+                {this.state.closed ? null : (
+                    <span data-show={!this.state.closed} {...others(new module.Props(), this.props)}
+                          className={classes}>
+                        <span className="tag-text">{this.props['children']}</span>
+                        {close}
+                    </span>
+                )}
+            </span>
+        )
+    }
+}
---
+++ src/tag/module.tsx
@@ -0,0 +1,39 @@
+export interface PropsInterface {
+    /**
+     * 是否可关闭
+     */
+    closable:boolean
+
+    /**
+     * 关闭前回调
+     */
+    beforeClose(close:Function):boolean
+
+    /**
+     * 关闭后回调
+     */
+    onClosed():void
+
+    [x:string]:any
+}
+
+export class Props implements PropsInterface {
+    closable = false
+
+    beforeClose(close) {
+        return true
+    }
+
+    onClosed() {
+    }
+}
+
+export interface StateInterface {
+    closing?:boolean
+    closed?:boolean
+}
+
+export class State implements StateInterface {
+    closing = false
+    closed = false
+}
